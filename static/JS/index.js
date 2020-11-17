ae7b61b90d67b4f2d8abbdd6c67190aff1f04207
wuyanzu <457975129@qq.com>
2020-11-17 17:51:08 +0800

    index（第一版js）

---
+++ static/JS/index.js
@@ -0,0 +1,23 @@
+function myfun(){
+                document.getElementById('nanzhuang').className = 'newbg';
+                document.getElementById('xianshi1').style.display = 'block'
+            }
+
+function myfu(){
+                document.getElementById('nanzhuang').className = ''
+                document.getElementById('xianshi1').style.display = 'none'
+            }
+function myfun1(){
+                document.getElementById('nvzhuang').className = 'newbg'
+                document.getElementById('xianshi1').style.display = 'block'
+            }
+function myfu1(){
+                document.getElementById('nvzhuang').className = ''
+                document.getElementById('xianshi1').style.display = 'none'
+            }
+function leibie(){
+                document.getElementById('xianshi1').style.display = 'block'
+}
+function leibi(){
+                document.getElementById('xianshi1').style.display = 'none'
+}
