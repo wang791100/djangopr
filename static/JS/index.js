6d5e3afe9b41fd7e1e76299841b858ea5cce7ee7
wuyanzu <457975129@qq.com>
2020-11-18 17:44:18 +0800

    model（jiangpin-date）

--- static/JS/index.js
+++ static/JS/index.js
@@ -8,16 +8,66 @@ function myfu(){
                 document.getElementById('xianshi1').style.display = 'none'
             }
 function myfun1(){
-                document.getElementById('nvzhuang').className = 'newbg'
-                document.getElementById('xianshi1').style.display = 'block'
+                document.getElementById('nvzhuang').className = 'new1bg'
+                document.getElementById('xianshi2').style.display = 'block'
             }
 function myfu1(){
                 document.getElementById('nvzhuang').className = ''
-                document.getElementById('xianshi1').style.display = 'none'
+                document.getElementById('xianshi2').style.display = 'none'
+            }
+function myfun2(){
+                document.getElementById('meizhuang').className = 'new2bg'
+                document.getElementById('xianshi3').style.display = 'block'
+            }
+function myfu2(){
+                document.getElementById('meizhuang').className = ''
+                document.getElementById('xianshi3').style.display = 'none'
+            }
+function myfun3(){
+                document.getElementById('shuma').className = 'new3bg';
+                document.getElementById('xianshi4').style.display = 'block'
+            }
+
+function myfu3(){
+                document.getElementById('shuma').className = ''
+                document.getElementById('xianshi4').style.display = 'none'
+            }
+function myfun4(){
+                document.getElementById('ertong').className = 'new4bg';
+                document.getElementById('xianshi5').style.display = 'block'
+            }
+
+function myfu4(){
+                document.getElementById('ertong').className = ''
+                document.getElementById('xianshi5').style.display = 'none'
             }
 function leibie(){
                 document.getElementById('xianshi1').style.display = 'block'
 }
 function leibi(){
                 document.getElementById('xianshi1').style.display = 'none'
+}
+function leibie1(){
+                document.getElementById('xianshi2').style.display = 'block'
+}
+function leibi1(){
+                document.getElementById('xianshi2').style.display = 'none'
+}
+function leibie2(){
+                document.getElementById('xianshi3').style.display = 'block'
+}
+function leibi2(){
+                document.getElementById('xianshi3').style.display = 'none'
+}
+function leibie3(){
+                document.getElementById('xianshi4').style.display = 'block'
+}
+function leibi3(){
+                document.getElementById('xianshi4').style.display = 'none'
+}
+function leibie4(){
+                document.getElementById('xianshi5').style.display = 'block'
+}
+function leibi4(){
+                document.getElementById('xianshi5').style.display = 'none'
 }
